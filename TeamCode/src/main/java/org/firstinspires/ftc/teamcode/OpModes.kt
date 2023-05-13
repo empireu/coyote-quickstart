934bc4757fd03744ab528102c4500f5cf537058d
empireu <rodney.mckay.meredith@gmail.com>
2023-05-14 00:27:49 +0300

    Implements concept composite node

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes.kt
@@ -5,14 +5,18 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.HardwareMap
 import empireu.coyote.*
 import org.firstinspires.ftc.teamcode.dashboard.CoyoteConfig.*
 import org.firstinspires.ftc.teamcode.dashboard.LocalizerConfig
 import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.lang.Integer.parseInt
 import java.net.URL
 import kotlin.math.PI
+import kotlin.math.abs
 
 @TeleOp(name = "Motor Test")
 class MotorTestOpMode: LinearOpMode() {
@@ -38,6 +42,7 @@ class ManualOpMode : LinearOpMode() {
         val lynxModules = hardwareMap.getAll(LynxModule::class.java)
         lynxModules.forEach { it.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL }
 
+        waitForStart()
         while (!isStopRequested){
             lynxModules.forEach { it.clearBulkCache() }
 
@@ -164,48 +169,63 @@ class CoyoteOpMode : LinearOpMode() {
 
         val drive = MecanumDrive(hardwareMap)
 
-        val node = loadNodeProject(
-            savedNodeProject.RootNodes,
-            BehaviorMapBuilder().also { b ->
-                b.add("Sequence", { ctx -> BehaviorSequenceNode(ctx.name, ctx.runOnce, ctx.childNodes) })
-                b.add("Selector", { ctx -> BehaviorSelectorNode(ctx.name, ctx.runOnce, ctx.childNodes) })
-                b.add("Success", { ctx -> BehaviorResultNode(ctx.name, ctx.runOnce, ctx.one(), BehaviorStatus.Success) })
-                b.add("Parallel", { ctx -> BehaviorParallelNode(ctx.name, ctx.runOnce, ctx.childNodes) })
+        val node: BehaviorNode
 
-                b.add(
-                    "Call",
-                    { ctx -> BehaviorCallNode(ctx.name, ctx.runOnce) },
+        try {
+            node = loadNodeProject(
+                savedNodeProject.RootNodes,
+                BehaviorMapBuilder().also { b ->
+                    b.add("Sequence", { ctx -> BehaviorSequenceNode(ctx.name, ctx.runOnce, ctx.childNodes) })
+                    b.add("Selector", { ctx -> BehaviorSelectorNode(ctx.name, ctx.runOnce, ctx.childNodes) })
+                    b.add("Success", { ctx -> BehaviorResultNode(ctx.name, ctx.runOnce, ctx.one(), BehaviorStatus.Success) })
+                    b.add("Parallel", { ctx -> BehaviorParallelNode(ctx.name, ctx.runOnce, ctx.childNodes) })
+                    b.add("Repeat", { ctx -> RepeatNode(ctx.name, ctx.runOnce, parseInt(ctx.savedData), ctx.one()) })
 
-                    // The call nodes need a second pass (to search for the target node):
-                    { ctx ->
-                        val target = ctx.project.behaviors.firstOrNull { it.root.name == ctx.createContext.savedData }
-                            ?: error("Failed to bind call node")
+                    b.add(
+                        "Call",
+                        { ctx -> BehaviorCallNode(ctx.name, ctx.runOnce) },
 
-                        ctx.node.bind(target.root)
+                        // The call nodes need a second pass (to search for the target node):
+                        { ctx ->
+                            val target = ctx.project.behaviors.firstOrNull { it.root.name == ctx.createContext.savedData }
+                                ?: error("Failed to bind call node")
+
+                            ctx.node.bind(target.root)
+                        }
+                    )
+
+                    b.add("Motion", { ctx -> BehaviorMotionNode(ctx, editorProject, drive) })
+
+                    registerNodes(b)
+                }.build()
+            ).behaviors
+                .filter { it.root.name == EntryNodeName }
+                .also {
+                    if(it.isEmpty()) {
+                        error("Failed to find root node $EntryNodeName")
                     }
-                )
 
-                b.add("Motion", { ctx -> BehaviorMotionNode(ctx, editorProject, drive) })
-            }.build()
-        ).behaviors
-            .filter { it.root.name == EntryNodeName }
-            .also {
-                if(it.isEmpty()) {
-                    error("Failed to find root node $EntryNodeName")
+                    if(it.size != 1) {
+                        error("Ambiguous root node $EntryNodeName")
+                    }
                 }
+                .first()
+                .root
 
-                if(it.size != 1) {
-                    error("Ambiguous root node $EntryNodeName")
-                }
-            }
-            .first()
-            .root
+
+
+        }
+        catch (t: Throwable) {
+            telemetry.addData("ERROR", t)
+            telemetry.update()
+            sleep(10000)
+            return
+        }
 
         val lynxModules = hardwareMap.getAll(LynxModule::class.java)
         lynxModules.forEach { it.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL }
 
         val context = BehaviorContext()
-
         waitForStart()
 
         fun needsStop() = isStopRequested || gamepad1.left_bumper || gamepad1.right_bumper || gamepad2.left_bumper || gamepad2.right_bumper
@@ -226,5 +246,96 @@ class CoyoteOpMode : LinearOpMode() {
         finally {
             drive.resetPower()
         }
+    }
+
+    private fun registerNodes(builder: BehaviorMapBuilder) {
+        builder.add("Arm", { ctx -> ArmNode(ctx.name, ctx.runOnce, ctx.savedData, hardwareMap) })
+    }
+
+    class ArmNode(name: String, runOnce: Boolean, stored: String, hardwareMap: HardwareMap): BehaviorCompositeNode(name, runOnce, stored) {
+        enum class ArmPreset(val joint: Int, val elevator: Int, val rotator: Double) {
+            Pickup(230, 20, 0.78),
+            Carry(230, 1000, 0.78),
+            Place(300, 20, 0.78)
+        }
+
+        companion object {
+            private const val ClawLClosed = 0.0
+            private const val ClawRClosed = 1.0
+            private const val ClawLOpen = 0.5
+            private const val ClawROpen = 0.75
+
+            private const val JointSpeed = 0.25
+            private const val ElevatorSpeed = 0.5
+
+            private const val ElevatorTolerance = 30
+            private const val JointTolerance = 50
+            private const val RotatorTolerance = 0.05
+            private const val ClawTolerance = 0.05
+        }
+
+        data class ArmOption(
+            val closed: Boolean,
+            val preset: ArmPreset
+        )
+
+        private val opt = loadStorage(ArmOption::class)
+        private val jointMotor = hardwareMap.dcMotor.get("MotorJoint")
+        private val elevatorMotor = hardwareMap.dcMotor.get("MotorElevator")
+        private val rotatorServo = hardwareMap.servo.get("ServoRotator")
+        private val claws = listOf(hardwareMap.servo.get("ServoClaw1"), hardwareMap.servo.get("ServoClaw2"))
+
+        private val c0Tgt: Double
+        private val c1Tgt: Double
+
+        init {
+            jointMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+            elevatorMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+            if(opt.closed){
+                c0Tgt = ClawLClosed
+                c1Tgt = ClawRClosed
+            }
+            else{
+                c0Tgt = ClawLOpen
+                c1Tgt = ClawROpen
+            }
+        }
+
+        override fun update(context: BehaviorContext): BehaviorStatus {
+            jointMotor.targetPosition = opt.preset.joint
+            elevatorMotor.targetPosition = opt.preset.elevator
+
+            jointMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
+            elevatorMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
+
+            jointMotor.power = JointSpeed
+            elevatorMotor.power = ElevatorSpeed
+
+            rotatorServo.position = opt.preset.rotator
+
+            if(
+                abs(opt.preset.elevator - elevatorMotor.currentPosition) > ElevatorTolerance ||
+                abs(opt.preset.joint - jointMotor.currentPosition) > JointTolerance ||
+                abs(opt.preset.rotator - rotatorServo.position) > RotatorTolerance)
+            {
+                // Wait for claw assembly to reach target before closing/opening claw
+
+                return BehaviorStatus.Running
+            }
+
+            claws[0].position = c0Tgt
+            claws[1].position = c1Tgt
+
+            if(
+                abs(c0Tgt - claws[0].position) < ClawTolerance &&
+                abs(c1Tgt - claws[1].position) < ClawTolerance)
+            {
+                return BehaviorStatus.Success
+            }
+
+            return BehaviorStatus.Running
+        }
+
     }
 }
